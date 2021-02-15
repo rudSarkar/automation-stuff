8539fed297e65737726a4a5193cccbb2bc1593a3
rudSarkar <rudrasarkar815@gmail.com>
2021-02-15 12:21:57 +0600

    few changes added

--- nmapBot.sh
+++ nmapBot.sh
@@ -8,17 +8,13 @@ date=$(date +%Y-%m-%d-%H-%M-%S)
 if [ "$*" == "" ]; then
     echo "$0 domainList.txt"
     exit 1
-else
-	nmap
 fi
 
 slackNotification() {
-	curl -F file=@$line-$date -F "initial_comment=Nmap scan finished for $line $date" -F channels=#<channel-id> -H "Authorization: Bearer xxxx-xxxx-xxxx-xxxx" https://slack.com/api/files.upload
+	curl -F file=@$line-$date -F "initial_comment=:white_check_mark: Nmap scan finished for $line" -F channels=#<channel-id> -H "Authorization: Bearer xxxx-xxxx-xxxx-xxxx" https://slack.com/api/files.upload
 }
 
-nmap() {
-	while IFS= read -r line || [ -n "$line" ]; do
-  		nmap -sV -A -p1- $line -o $line-$date
-  		slackNotification
-	done < $filename
-}
+while IFS= read -r line || [ -n "$line" ]; do
+	nmap -sV -A -p1- $line > $line-$date
+	slackNotification
+done < $filename
